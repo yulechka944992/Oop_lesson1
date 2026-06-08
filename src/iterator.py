7874d47136a267bf1ff7370c98fd091e71dd3925
Юлия Кряжева <yulechka944992@gmail.com>
2026-06-09 01:59:42 +0900

    add new module

--- src/iterator.py
+++ src/iterator.py
@@ -1,20 +1,23 @@
 from src.category import Category
 from src.product import Product
 
+
 class CategoryIterator:
+    """Вспомогательный класс для итерации товаров в заданной категории"""
+
     def __init__(self, category):
         self.category = category
         self.index = -1
-
 
     def __iter__(self):
         self.index = -1
         return self
 
     def __next__(self):
-        if self.index + 1 < len(self.category.products):
+        products_list = self.category.get_products_list()
+
+        if self.index + 1 < len(products_list):
             self.index += 1
-            return self.category.products[self.index]
+            return products_list[self.index]
         else:
             raise StopIteration
-
