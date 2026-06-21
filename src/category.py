6026d313b93c93fe601f8680ad4ce0fef27b82c4
Юлия Кряжева <yulechka944992@gmail.com>
2026-06-22 01:29:39 +0900

    change add funcs in modules

--- src/category.py
+++ src/category.py
@@ -27,8 +27,11 @@ class Category:
 
     def add_product(self, product: Product):
         """Добавление продукта в список"""
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self):
--- src/product.py
+++ src/product.py
@@ -19,7 +19,9 @@ class Product:
 
     def __add__(self, other):
         """Метод для получения суммы всех товаров на складе"""
-        return self.__price * self.quantity + other.__price * other.quantity
+        if type(self) == type(other):
+            return self.__price * self.quantity + other.__price * other.quantity
+        raise TypeError
 
     @classmethod
     def new_product(cls, product_dict, products=None):
