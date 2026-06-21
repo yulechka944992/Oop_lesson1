d823ec1373015ff2e6e49f7eccbbbc8be12fd659
Юлия Кряжева <yulechka944992@gmail.com>
2026-06-22 01:29:03 +0900

    add imports for main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.category import Category
+from src.lawngrass import LawnGrass
 from src.product import Product
+from src.smartphone import Smartphone
 
 if __name__ == '__main__':
     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
