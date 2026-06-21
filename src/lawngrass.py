244b13c6f3348a55fd5946fd52ba85fc522220e6
Юлия Кряжева <yulechka944992@gmail.com>
2026-06-22 01:29:59 +0900

    add new classes

---
+++ src/lawngrass.py
@@ -0,0 +1,9 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
